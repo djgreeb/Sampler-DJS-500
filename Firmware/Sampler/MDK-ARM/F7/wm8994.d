feeacb1d7cd5811cc19f25a309efe614ef6318ac
Andrei <greeb-mail@mail.ru>
2023-02-09 17:24:34 +0300

    Add files via upload

---
+++ Firmware/Sampler/MDK-ARM/F7/wm8994.d
@@ -0,0 +1,4 @@
+f7\wm8994.o: ..\Src\wm8994.c
+f7\wm8994.o: ../Inc/wm8994.h
+f7\wm8994.o: ..\Drivers\BSP\Components\rk043fn48h\../Common/audio.h
+f7\wm8994.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
